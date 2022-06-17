1d149ca4df640aeb14d0189d9867ca7fb4abbd51
bl4ko <go3969@student.uni-lj.si>
2022-06-17 16:47:38 +0200

    Tasks routes; sorting,pagination, and filtering options added

--- src/models/user.js
+++ src/models/user.js
@@ -48,6 +48,8 @@ const userSchema = new mongoose.Schema({
             required: true
         }
     }]
+}, {
+    timestamps: true
 });
 
 // Virtual property = relationship between two entities (user and tasks)
--- src/routers/task.js
+++ src/routers/task.js
@@ -17,9 +17,32 @@ router.post('/tasks', auth, async (req, res) => {
     }
 });
 
+// GET /tasks?completed=true || completed=false
+// GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt:desc  // sort by field
 router.get('/tasks', auth, async (req, res) => {
+    const match = {};
+    const sort = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(":");
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
+
     try {
-        await req.user.populate('tasks');
+        await req.user.populate({
+            path: 'tasks',
+            match: match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        });
         res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send(e);
